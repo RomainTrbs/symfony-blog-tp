516e3a5e116cc1e76333a816595b9de226b27db1
Romain Tirbois <tirbrom@ndlp.fr>
2023-03-28 16:32:49 +0200

    catalog

--- src/DataFixtures/PageFixtures.php
+++ src/DataFixtures/PageFixtures.php
@@ -21,61 +21,52 @@ class PageFixtures extends Fixture
         $home = new Page();
         $home->setTitle($this->translator->trans("Home"));
         $home->setText("<h1>" . $this->translator->trans("Home") . "</h1>");
-        $home->setNumber(1);
         $manager->persist($home);
 
         $centre = new Page();
         $centre->setTitle($this->translator->trans("Formation Center"));
         $centre->setText("<h1>" . $this->translator->trans("Formation Center") . "</h1>");
-        $centre->setNumber(2);
         $manager->persist($centre);
 
         $catalogue = new Page();
         $catalogue->setTitle($this->translator->trans("Catalogue"));
         $catalogue->setText("<h1>" . $this->translator->trans("Catalogue") . "</h1>");
-        $catalogue->setNumber(3);
         $manager->persist($catalogue);
 
         $rate = new Page();
         $rate->setTitle($this->translator->trans("Rate a Formation"));
         $rate->setText("<h1>" . $this->translator->trans("Rate a Formation") . "</h1>");
-        $rate->setNumber(4);
+
         $manager->persist($rate);
 
         $tuto = new Page();
         $tuto->setTitle($this->translator->trans("Tutos"));
         $tuto->setText("<h1>" . $this->translator->trans("Tutos") . "</h1>");
-        $tuto->setNumber(5);
         $manager->persist($tuto);
 
         $certif = new Page();
         $certif->setTitle($this->translator->trans("Qualiopi Certification"));
         $certif->setText("<h1>" . $this->translator->trans("Qualiopi Certification") . "</h1>");
-        $certif->setNumber(6);
         $manager->persist($certif);
 
         $contact = new Page();
         $contact->setTitle($this->translator->trans("Contact"));
         $contact->setText("<h1>" . $this->translator->trans("Contact") . "</h1>");
-        $contact->setNumber(7);
         $manager->persist($contact);
 
         $infos = new Page();
         $infos->setTitle($this->translator->trans("Infos légales"));
         $infos->setText("<h1>" . $this->translator->trans("Infos légales") . "</h1>");
-        $infos->setNumber(8);
         $manager->persist($infos);
 
         $regles = new Page();
         $regles->setTitle($this->translator->trans("Règles de confidentialité"));
         $regles->setText("<h1>" . $this->translator->trans("Règles de confidentialité") . "</h1>");
-        $regles->setNumber(9);
         $manager->persist($regles);
 
         $reserve = new Page();
         $reserve->setTitle($this->translator->trans("Réservé"));
         $reserve->setText("<h1>" . $this->translator->trans("Réservé") . "</h1>");
-        $reserve->setNumber(10);
         $manager->persist($reserve);
 
         $manager->flush();
